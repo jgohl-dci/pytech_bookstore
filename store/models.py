f31eeca3bee848db1ff1d27492d3053be52c3f41
jgohl-dci <johannes.gohl@dci-student.org>
2024-02-14 00:34:30 +0100

    fin fr

--- store/models.py
+++ store/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.utils.text import slugify
 
 
-GENRE_CHOICES = (
-        ("0", "None"),
-        ("1", "Romance"),
-        ("2", "Sci-Fi"),
-        ("3", "Fantasy"),
-        ("4", "Drama"),
-        ("5", "Poetry"),
-        ("6", "Horror"),
-    )
+# GENRE_CHOICES = (
+#         ("0", "None"),
+#         ("1", "Romance"),
+#         ("2", "Sci-Fi"),
+#         ("3", "Fantasy"),
+#         ("4", "Drama"),
+#         ("5", "Poetry"),
+#         ("6", "Horror"),
+#     )
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, unique=True)
 
 
 # Create your models here.
@@ -18,7 +22,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
     email = models.EmailField(max_length=100, unique=True, db_index=True)
-    genre = models.CharField(max_length=30, choices=GENRE_CHOICES, default="0")
+    #  genre = models.ManyToManyField(Genre)
     date_of_birth = models.DateField(null=False)
     created_on = models.DateField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
@@ -36,7 +40,7 @@ class Author(models.Model):
         get_latest_by = "created_on"  # Author.objects.latest()
         indexes = [
             models.Index(fields=["last_name", "first_name"], name="full_name_idx"),
-            models.Index(fields=["genre"], name="genre_idx"),
+            # models.Index(fields=["genre"], name="genre_idx"),
         ]
 
     def __str__(self):
@@ -48,7 +52,7 @@ class Book(models.Model):
     author = models.CharField(max_length=200, null=False)
     summary = models.TextField(max_length=1000, null=False)
     isbn = models.CharField(max_length=13, unique=True, help_text="13 char string - ISBN Number")
-    genre = models.CharField(max_length=30, choices=GENRE_CHOICES, default="0")
+    genre = models.ManyToManyField(Genre)
     date_of_publishing = models.DateField()
     in_stock = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=False)  # 999.99
