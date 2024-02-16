8fa33a8e3a58be824858996e26787e26afdce109
jgohl-dci <johannes.gohl@dci-student.org>
2024-02-16 12:15:24 +0100

    templates

--- store/views_example2.py
+++ store/views_example2.py
@@ -1,19 +1,29 @@
 from django.http import HttpResponse
 from .models import Author, Book, Genre
 from django.views import View
-#  from django.urls import reverse
-from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(request):
-    number_of_books = Book.objects.all().count()
-    return render(request, "store/index.html", {"num_of_books": number_of_books})
+    authors_link = f"<a href={reverse('store:author_list')}>Authors</a>"
+    books_link = f"<a href={reverse('store:book_list')}>Books</a>"
+    genres_link = f"<a href={reverse('store:genre_list')}>Genres</a>"
+
+    content = f"<h3>Bookstore</h3><ul><li>{authors_link}</li> <li>{books_link}</li> <li>{genres_link}</li></ul>"
+
+    return HttpResponse(content)
 
 
 class AuthorListView(View):
     def get(self, request):
         authors = Author.objects.all()
-        return render(request, "store/author_list.html", {"authors": authors})
+        content = "<h3>Authors:</h3>"
+
+        for author in authors:
+            new_author = f"<li>{author.first_name} {author.last_name}</li>"
+            content += new_author
+
+        return HttpResponse(content)
 
 
 class BookListView(View):
