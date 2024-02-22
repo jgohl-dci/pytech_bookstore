bba9ebd18ff00d1ddb491d4cb167a247e5988873
jgohl-dci <johannes.gohl@dci-student.org>
2024-02-22 11:08:43 +0100

    add user

---
+++ user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    bio = models.TextField()
